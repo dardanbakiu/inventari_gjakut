b161c47354e4c95f4b01350394e0d4d4488f9222
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-02-10 23:47:59 +0100

    lista_marresve
    Lista e marresve db

--- router/listaDhuruesveRoute.js
+++ router/listaDhuruesveRoute.js
@@ -6,8 +6,6 @@ router.get('/lista_dhuruesve', (req, res) => {
     db.execute("SELECT * FROM shtodhurues", (error,rows,fields)=> {
         res.render('lista_dhuruesve', {rows:rows});
     });
-    
-    
 });
 
 exports.route = router; 
